9a1dc8067953db5476accb7e35132f1533bd3679
ceschae <ceschae@gmail.com>
2015-06-16 11:30:58 -0700

    added comments to euler 001

--- ProjectEuler/Euler001.java
+++ ProjectEuler/Euler001.java
@@ -1,3 +1,13 @@
+/**
+ * Project Euler: Problem 1
+ * 'Multiples of 3 and 5'
+ * Finds the sum of all multiples of 3 or 5 below MAX.
+ *
+ * @author  Caitlin Schaefer
+ * @version 2.0
+ * @since   16-June-2015
+ */
+
 public class Euler001 {
    public static final int MAX = 1000;
    
@@ -6,6 +16,11 @@ public class Euler001 {
       version2();
    }
    
+   /**
+    * First attempt. Brute force method of adding number
+    * to sum if it is a mutliple of 3 or 5, checking up 
+    * to MAX. Prints solution to console.
+    */
    public static void version1() {
       int sum = 0;
       for (int i = 0; i < MAX; i++) {
@@ -16,11 +31,25 @@ public class Euler001 {
       System.out.println(sum);
    }
    
+   /**
+    * Second attempt (best solution). Finds sums of numbers
+    * divisible by 3 and 5, and subtracts sum of numbers
+    * divisible by 15 (to eliminate repeats). Prints solution
+    * to console.
+    */
    public static void version2() {
       int sum = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
       System.out.println(sum);
    }
    
+   /**
+    * Calculates sum of numbers up to MAX that are divisible by n.
+    * 
+    * @param n This is the number of which multiples should be added
+    * to the suum.
+    * @return int This is the sum of numbers up to MAX that are 
+    * divisible by n.
+    */
    private static int sumDivisibleBy(int n) {
       int p = MAX / n;
       return n * p * (p + 1) / 2;
