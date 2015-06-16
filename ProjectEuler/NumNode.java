e22e1e832b89c12f0927070434cb2968506ee135
ceschae <ceschae@gmail.com>
2015-06-16 13:17:05 -0700

    added comments to NumNode class

--- ProjectEuler/NumNode.java
+++ ProjectEuler/NumNode.java
@@ -1,7 +1,17 @@
+/**
+ * This class defines a node, representing a long and 
+ * a reference to a next node. 
+ */
 public class NumNode {
 	public long	data;
 	public NumNode	next;
 	
+   /**
+    * This constructs one node. 
+    * 
+    * @param data The long that the node represents
+    * @param next The reference to the next node in the list
+    */
 	public NumNode(long data, NumNode next) {
 		this.data =	data;
 		this.next =	next;
