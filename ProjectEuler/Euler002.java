18d5d3031da36ab8b54d0205e141bd6b6dafe29f
ceschae <ceschae@gmail.com>
2015-06-16 11:40:26 -0700

    added comments to 002, recompiled 001

--- ProjectEuler/Euler002.java
+++ ProjectEuler/Euler002.java
@@ -1,13 +1,28 @@
+/**
+ * Project Euler: Problem 2
+ * 'Even Fibonacci Numbers'
+ * Finds the sum of evan Fibonacci numbers up to MAX. 
+ *
+ * @author  Caitlin Schaefer
+ * @version 2.0
+ * @since   16-June-2015
+ */
+
 public class Euler002 {
    public static final int MAX = 4000000;
-   public static final int N_1 = 1;
-   public static final int N_2 = 2;
+   public static final int N_1 = 1; // starting term n_1
+   public static final int N_2 = 2; // starting term n_2
    
    public static void main(String[] args) {
       version1();
       version2();
    }
    
+   /**
+    * First attempt. Brute force calculates every Fibonacci
+    * number, and adds to the sum if it is even. Prints solution
+    * to console.
+    */
    public static void version1() {
       int sum = 0;
       int n1 = N_1;
@@ -27,6 +42,11 @@ public class Euler002 {
       System.out.println(sum);
    }
    
+   /** 
+    * Second attempt (best solution). Calculates each successive even
+    * Fibonacci number with knowledge that F(n) = 4*F(n-1) + F(n-2),
+    * summing these up. Prints solution to console.
+    */
    public static void version2() {
       int n1 = 0;
       int n2 = N_2;
