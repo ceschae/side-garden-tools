84b0ea237381b55e53e9e526f53310958c181309
ceschae <ceschae@gmail.com>
2015-06-16 13:59:34 -0700

    added comments to 004

--- ProjectEuler/Euler004.java
+++ ProjectEuler/Euler004.java
@@ -1,8 +1,27 @@
+/**
+ * Project Euler: Problem 4
+ * 'Largest Palindrome Product'
+ * Finds the largest palindromic number that can be made from 
+ * two 3-digit numbers.
+ *
+ * @author  Caitlin Schaefer
+ * @version 2.0
+ * @since   16-June-2015
+ */
+
 public class Euler004 {
    public static void main(String[] args) {
       version2();
    }
    
+   /**
+    * Second attempt (best solution). Stems from criteria that 
+    * one of the numbers must be a multiple of 11 for the result to be
+    * a palindrome, so we can skip many pairings. We can also stop early
+    * if we find a palindrome for the first number 'a' and all subsequent
+    * numbers 'b' for a pairing, given decreasing 'a' and 'b'. Prints
+    * solution to console. 
+    */
    public static void version2() {
       int largestPalindrome = 0;
       int a = 999;
@@ -30,6 +49,11 @@ public class Euler004 {
       System.out.println(largestPalindrome);
    }
    
+   /**
+    * First attempt. This attempt brute forces all the mutliplications
+    * of every pair of 3-digit numbers, and keeps track of a max
+    * palindrome. Prints result to console. 
+    */
    public static void version1() {
       int max = 0;
       for (int i = 999; i >= 100; i--) {
@@ -38,8 +62,6 @@ public class Euler004 {
             if (isPalindrome("" + result)) {
                if (result > max) {
                   max = result;
-                  System.out.println("i = " + i);
-                  System.out.println("j = " + j);
                }
             }
          }
@@ -47,7 +69,14 @@ public class Euler004 {
       System.out.println(max);
    }
    
-   public static boolean isPalindrome(String str) {
+   /**
+    * Returns whether or not str is a Palindrome (same
+    * forwards as it is backwards).
+    *
+    * @param str The String to be checked if it is a palindrome.
+    * @return boolean True if str is a palindrome, false otherwise.
+    */
+   private static boolean isPalindrome(String str) {
       for (int i = 0; i < str.length() / 2; i++) {
          if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
             return false;
