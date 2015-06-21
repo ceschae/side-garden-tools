a31f46e9e722373149f759ec0e79d2d9695cf41c
ceschae <ceschae@gmail.com>
2015-06-20 19:16:01 -0700

    finished 1.0 of reader

--- ReadingLevel/UlyssesReader.java
+++ ReadingLevel/UlyssesReader.java
@@ -13,14 +13,26 @@ public class UlyssesReader {
 	public static void main(String[] args) {
 		Scanner document = new Scanner(new File("Ulysses.txt"));
 		Map<String, Integer> wordCount = new HashMap<String, Integer>();
+		int totalWordCount = 0;
 		while (document.hasNext()) {
 			String word = document.next();
 			if (!wordCount.containsKey(word)) {
 				wordCount.put(word, 0);
 			}
 			wordCount.put(word, wordCount.get(word) + 1);
+			totalWordCount++;
 		}
+		System.out.println("Total word count: " + totalWordCount);
 		
+		// 20 = sum(word difficulties) * length of text * constant
+		// word difficulty = frequency * length
+		double wordDifficultiesSum = 0.0;
+		for (String word : wordCount.keySet()) {
+			wordDifficultiesSum += wordCount.get(word) * word.length();
+		}
+		System.out.println("Sum of all word difficulties: " + wordDifficultiesSum);
 		
+		double constant = TARGET_DIFFICULTY / wordDifficultiesSum / totalWordCount;
+		System.out.println("Projected constant: " + constant);
 	}
 }
